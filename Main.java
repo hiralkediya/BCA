b2180d2227328a32a58ce7b934a5feef3598c769
hiral <hiral@gmail.com>
2025-07-20 20:49:08 +0530

    r2

--- Main.java
+++ Main.java
@@ -6,6 +6,12 @@ public class Main{
         System.out.println(message);
     
     
+    if (number > 0) {
+        System.out.println("The number is positive.");
+    } else if (number < 0) {
+        System.out.println("The number is negative.");
+    } else {
+        System.out.println("The number is zero.");
     
     }
 }
