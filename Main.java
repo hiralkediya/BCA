8e1d544f92351a398058aec7342797ba36da935c
hiral <hiral@gmail.com>
2025-07-20 18:32:47 +0530

    r2

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    int number = 42;
+        String message = "The answer is: " + number;
+        System.out.println(message);
+    
+    
+    
+    }
+}
